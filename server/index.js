559a45a9e189a620a0177a9c26c6d2a84541312e
roshangaud3096-rgb <roshangaud3096@gmail.com>
2026-03-24 22:02:40 +0530

    update signup page

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 // }))
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5175"],
     credentials: true
 }));
 app.use(cookieParser())
